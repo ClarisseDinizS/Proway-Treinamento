feb31c8bf42206a404b9800452ae38820dfe6734
ClarisseDinizS <dinizclarisse.cd@gmail.com>
2023-09-07 14:21:10 -0300

    Criação das rotas e funcionalidades da barra de pesquisa

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IProduto, produtos } from '../produto';
 import { ProdutosService } from '../produtos.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-produtos',
@@ -11,10 +12,22 @@ export class ProdutosComponent {
     produtos: IProduto[] | undefined;
 
 
-    constructor(private produtosService: ProdutosService){
+    constructor(private produtosService: ProdutosService, 
+      private route: ActivatedRoute){
     }
 
     ngOnInit(): void{
-      this.produtos =  this.produtosService.getAll();
+      const produtos = this.produtosService.getAll();
+      this.route.queryParamMap.subscribe(params => {
+      const descricao = params.get('descricao')?.toLowerCase();
+
+        if(descricao){
+          this.produtos = produtos.filter(produtos => produtos.descricao.toLowerCase().includes(descricao));
+          return;
+        }
+
+        this.produtos = produtos;
+        
+      });
     }
 }
